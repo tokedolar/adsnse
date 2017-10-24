06f2e70e12e7b7861110799b92d31b865262e452
tokedolar <33062155+tokedolar@users.noreply.github.com>
2017-10-24 23:24:40 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -26,7 +26,7 @@ if(x=="1"){xxx="/2017/10/luis-suarez.html"}
 if(x=="2"){xxx="/2017/10/neymar.html"}
 if(x=="3"){xxx="/2017/10/david-beckham.html"}
 if(x=="4"){xxx="/2017/10/cristiano-ronaldo.html"}
-if(x=="5"){xxx="/2017/08/2017/10/lionel-messi.html"}a_to_ve[i].href="https://footballskillsl.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/10/lionel-messi.html"}a_to_ve[i].href="https://footballskillsl.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
